5fdc66037308f7122e4469d2eee3c8cb58376e81
Aaron.Nacua <alnacua@gmail.com>
2017-12-18 16:11:34 -0600

    step 7 complete

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import store from "./store";
+import { HashRouter } from "react-router-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import "./index.css";
+
+ReactDOM.render(
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
+
+  document.getElementById("root")
+);
 registerServiceWorker();
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
+// import "./index.css";
 
+// import store from "./store";
+// import { Provider } from "react-redux";
+// import { HashRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <HashRouter>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </HashRouter>,
+//   document.getElementById("root")
+// );
+// registerServiceWorker();
